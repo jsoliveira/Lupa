7ea390cb30391dddc0613bcf019054bcb00cea6b
Junior Nakamura <Nakamura@Mac-mini-de-GRITO.local>
2017-06-02 22:11:03 -0300

    Término de Layout das Notificações

--- src/pages/notificacoes/notificacoes.ts
+++ src/pages/notificacoes/notificacoes.ts
@@ -14,6 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'notificacoes.html',
 })
 export class NotificacoesPage {
+  current: number = 27;
+  max: number = 50;
+  stroke: number = 5;
+  radius: number = 20;
+  semicircle: boolean = false;
+  rounded: boolean = false;
+  responsive: boolean = false;
+  clockwise: boolean = true;
+  color: string = '#45ccce';
+  background: string = '#eaeaea';
+  duration: number = 800;
+  animation: string = 'easeOutCubic';
+  animationDelay: number = 0;
+  animations: string[] = [];
+  gradient: boolean = false;
+  realCurrent: number = 0;  
 
   constructor(
     public navCtrl: NavController, 
@@ -30,5 +46,22 @@ export class NotificacoesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad Notificacoes');
   }
+
+
+  getOverlayStyle() {
+    let isSemi = this.semicircle;
+    let transform = (isSemi ? '' : 'translateY(-50%) ') + 'translateX(-50%)';
+
+    return {
+      'top': isSemi ? 'auto' : '50%',
+      'bottom': isSemi ? '5%' : 'auto',
+      'left': '50%',
+      'transform': transform,
+      '-moz-transform': transform,
+      '-webkit-transform': transform,
+      'font-size': this.radius / 1.5 + 'px'
+    };
+  }
+
 
 }
